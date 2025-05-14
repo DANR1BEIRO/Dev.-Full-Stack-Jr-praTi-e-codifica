da1ce6843cabe4646d0a7b04369a7f14a5c70d52
DANR1BEIRO <danribeiro001@gmail.com>
2025-05-14 00:29:46 -0300

    Aula 9: Funções

--- JavaScript/04_Function/function.js
+++ JavaScript/04_Function/function.js
@@ -42,7 +42,7 @@ console.log(sum([1, 2, 3]));
 
 function largest(arr) {
     let largest = arr[0];
-    
+    a
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > largest) {
             largest = arr[i];
